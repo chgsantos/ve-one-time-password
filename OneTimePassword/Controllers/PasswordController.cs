fecf98e86e96204068395ef866a694ce1c3ae789
Caio Henrique <caio@sys10.com.br>
2016-02-09 20:04:06 -0200

    User model.
    Password controller.

---
+++ OneTimePassword/Controllers/PasswordController.cs
@@ -0,0 +1,45 @@
+﻿using OneTimePassword.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneTimePassword.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // Declare the list of Users
+        private static List<User> _users;
+
+        /// <summary>
+        /// Initializes the app with some users
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult Initialize()
+        {
+            return Json(true);
+        }
+
+        /// <summary>
+        /// Generates a password for a given User ID
+        /// </summary>
+        /// <param name="userId">User Id</param>
+        /// <returns>The generated password for the given User ID</returns>
+        public JsonResult GeneratePassword(Guid userId)
+        {
+            return Json("123456");
+        }
+
+        /// <summary>
+        /// Checks the validity of a given User ID and password
+        /// </summary>
+        /// <param name="userId">User Id</param>
+        /// <param name="password">Password</param>
+        /// <returns>Validity of the password</returns>
+        public JsonResult ValidatePassword(Guid userId, String password)
+        {
+            return Json(true);
+        }
+    }
+}
---
+++ OneTimePassword/Models/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OneTimePassword.Models.Entities
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    public class User
+    {
+        // Unique Identifier
+        public Guid Id { get; set; }
+
+        // One time password
+        public String Password { get; set; }
+
+        // Password generated date
+        public DateTime? PasswordDate { get; set; }
+    }
+}
